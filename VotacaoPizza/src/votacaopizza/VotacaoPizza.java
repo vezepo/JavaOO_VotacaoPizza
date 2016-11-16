ce49c79f7b40152c2a74849ac404e34524ce9210
1617604 <1617604@034lab04004.up.edu>
2016-11-16 19:57:49 -0200

    deleted comments

--- VotacaoPizza/src/votacaopizza/VotacaoPizza.java
+++ VotacaoPizza/src/votacaopizza/VotacaoPizza.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package votacaopizza;
 
-/**
- *
- * @author 1617604
- */
+
 public class VotacaoPizza {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
